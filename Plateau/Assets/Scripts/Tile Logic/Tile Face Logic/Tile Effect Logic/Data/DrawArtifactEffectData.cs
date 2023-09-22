6b917b7a2afe8f12e004f6ae535528056f8a889f
Minotoror29 <matthieu.boissel@laposte.net>
2023-09-22 15:42:02 +0200

    Draw Artifact Effect

---
+++ Plateau/Assets/Scripts/Tile Logic/Tile Face Logic/Tile Effect Logic/Data/DrawArtifactEffectData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Effect/Draw Artifacts")]
+public class DrawArtifactEffectData : EffectData
+{
+    public int artifactAmount;
+
+    public override Effect Effect(TableManager tableManager)
+    {
+        return new DrawArtifactEffect(tableManager, this);
+    }
+}
---
+++ Plateau/Assets/Scripts/Tile Logic/Tile Face Logic/Tile Effect Logic/Tile Effects/DrawArtifactEffect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawArtifactEffect : Effect
+{
+    private int _artifactAmount;
+
+    public DrawArtifactEffect(TableManager tableManager, DrawArtifactEffectData data) : base(tableManager)
+    {
+        _artifactAmount = data.artifactAmount;
+    }
+
+    public override void Activate()
+    {
+        TableManager.Player.DrawArtifacts(_artifactAmount);
+        ResolveEffect();
+    }
+}
