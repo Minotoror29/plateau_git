68cdb019760516cefa873fd9f2e27a7e0f3b0129
Minotoror29 <matthieu.boissel@laposte.net>
2023-09-22 15:56:30 +0200

    Additional Movement Effect

---
+++ Plateau/Assets/Scripts/Effect Logic/Data/AdditionalMovementEffectData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Effect/Additional Movement")]
+public class AdditionalMovementEffectData : EffectData
+{
+    public override Effect Effect(TableManager tableManager)
+    {
+        return new AdditionalMovementEffect(tableManager);
+    }
+}
---
+++ Plateau/Assets/Scripts/Effect Logic/Effects/AdditionalMovementEffect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditionalMovementEffect : Effect
+{
+    public AdditionalMovementEffect(TableManager tableManager) : base(tableManager)
+    {
+    }
+
+    public override void Activate()
+    {
+        //A revoir lorsque le système multijoueur ou le système de sorts seront ajoutés
+        ResolveEffect();
+    }
+}
